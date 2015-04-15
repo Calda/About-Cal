5c61846f664d9ac8b8a02b91dcc934dcab2b5510
Calda <calstephens98@gmail.com>
2015-04-15 14:08:20 -0400

    Work on collection views

--- About Cal/ViewController.swift
+++ About Cal/ViewController.swift
@@ -8,37 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    @IBOutlet weak var iconView: UIView!
-    @IBOutlet weak var iconImage: UIImageView!
-    @IBOutlet weak var iconBorder: UIImageView!
     @IBOutlet weak var contentText: UILabel!
     @IBOutlet weak var textWidth: NSLayoutConstraint!
+    //collection view of section icons
+    @IBOutlet weak var sectionIcons: UICollectionView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //create rounded mask and add border
-        iconImage.layer.cornerRadius = iconImage.frame.width / 2
-        iconImage.clipsToBounds = true
-        iconBorder.layer.cornerRadius = iconBorder.frame.width / 2
-        let borderColor = UIColor(red: 68/255, green: 103/255, blue: 123/255, alpha: 1.0).CGColor
-        iconBorder.layer.borderColor = borderColor
-        iconBorder.layer.borderWidth = 2.5
-        
-        //create and draw gradient
-        let iconFrame = iconView.frame
-        let iconCenter = CGPointMake(CGRectGetMidX(iconFrame), CGRectGetMidY(iconFrame))
-        let pageWidth = self.view.frame.width
-        let nextColor = UIColor(red: 123/255, green: 68/255, blue: 123/255, alpha: 1.0).CGColor
-        
-        let nextGradient = CAGradientLayer()
-        nextGradient.frame = CGRectMake(CGRectGetMidX(iconFrame) + iconBorder.bounds.width/2 - 2, CGRectGetMidY(iconFrame) - 1.25, pageWidth, 2.5)
-        nextGradient.colors = [borderColor, nextColor] as NSArray
-        nextGradient.startPoint = CGPointMake(0.0, 0.5)
-        nextGradient.endPoint = CGPointMake(1.0, 0.5)
-        self.view.layer.insertSublayer(nextGradient, below: iconBorder.layer)
         
         //set proper width for text fields
         textWidth.constant = self.view.bounds.width - 30
@@ -50,6 +29,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("sectionIcon", forIndexPath: indexPath) as! IconCell
+        cell.decorateCell(70, collectionView)
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return (section == 0 ? 10 : 0)
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(100, 100)
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: self.view.frame.width/2 - 55, bottom: 0, right: self.view.frame.width/2 - 55)
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 70
+    }
+    
 }
 
